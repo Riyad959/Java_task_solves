65d5c1bd2bc643ca0922816e7e1443f3d4f79981
Riyad959 <riyadul15-4828@diu.edu.bd>
2023-01-31 19:14:20 +0600

    1st_commit

---
+++ Task2_prob1.java
@@ -0,0 +1,14 @@
+
+//Riyadul islam
+//ID-221-15-4828
+//Section-H
+import java.util.Scanner;
+
+public class Task2_prob1 {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    System.out.print("Enter your name: ");
+    String name = scan.nextLine();
+    System.out.println("Greetings! " + name + "!");
+  }
+}
